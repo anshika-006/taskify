d8effff048eaf4851b9fcfabcd96b0d1b77300ec
Anshika Jain <anshikaryan2006@gmail.com>
2025-07-26 00:32:18 +0530

    linked

--- todo-backend/src/index.ts
+++ todo-backend/src/index.ts
@@ -3,7 +3,10 @@ import cors from 'cors';
 import router from './routes'
 
 const app=express()
-app.use(cors())
+app.use(cors({
+  origin: 'https://anshika.vaamsolution.com',
+  credentials: true,
+}));
 app.use(express.json())
 
 app.use('/api/v1',router)
